11eeae726cb56d8ba713245a8769594a0891190f
Thiago Leal <lealthiago435@gmail.com>
2023-08-05 16:02:55 -0300

    Adicionando confirgurações de política CORS

--- api/serveidor.js
+++ api/serveidor.js
@@ -3,8 +3,12 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+const cors = require('cors');
+server.use(cors());
+
 server.use(middlewares);
 server.use(router);
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`JSON Server is running on port ${PORT}`);
